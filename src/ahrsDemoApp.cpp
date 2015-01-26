e3a2bba2d7064c8e4c8ec3137d202646db936538
Spencer Russell <spencer.f.russell@gmail.com>
2015-01-26 13:25:09 -0500

    initial project skeleton

---
+++ src/ahrsDemoApp.cpp
@@ -0,0 +1,61 @@
+#include "ahrsDemoApp.h"
+
+//--------------------------------------------------------------
+void ahrsDemoApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ahrsDemoApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
